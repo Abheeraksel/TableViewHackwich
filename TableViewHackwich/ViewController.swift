dd0c2a6e8ea9105377de1527eb4538ec474c12a4
GONZALEZ,AG AXEL <rl190076@rlas-macair-33.local>
2018-10-29 12:09:28 -0500

    Initial Commit

---
+++ TableViewHackwich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewHackwich
+//
+//  Created by GONZALEZ,AG AXEL on 10/29/18.
+//  Copyright © 2018 GONZALEZ,AG AXEL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TableViewHackwichTests/TableViewHackwichTests.swift
@@ -0,0 +1,34 @@
+//
+//  TableViewHackwichTests.swift
+//  TableViewHackwichTests
+//
+//  Created by GONZALEZ,AG AXEL on 10/29/18.
+//  Copyright © 2018 GONZALEZ,AG AXEL. All rights reserved.
+//
+
+import XCTest
+@testable import TableViewHackwich
+
+class TableViewHackwichTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
